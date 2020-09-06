f2f215866bab2807b4054c0f172eebf751d0e680
Lucas <martinslf@taginfraestrutura.com.br>
2020-09-06 08:54:11 -0300

    added cors

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const app = express();
 //define o dominio de origem para consumo do servico
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 // app.use(
 //   cors({
 //     origin: 'http://localhost:8080',
