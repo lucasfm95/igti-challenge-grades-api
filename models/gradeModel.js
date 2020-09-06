04a5e4951f4d9267de6410f25b9205acb0768795
Lucas <martinslf@taginfraestrutura.com.br>
2020-09-06 09:29:45 -0300

    fixed model

--- models/gradeModel.js
+++ models/gradeModel.js
@@ -22,6 +22,14 @@ export default (mongoose) => {
         }
     });
 
+    schema.method("toJSON", function () {
+        const { __v, _id, ...object } = this.toObject();
+
+        object.id = _id;
+
+        return object;
+    });
+
     const Grade = mongoose.model("grades", schema);
 
     return Grade;
